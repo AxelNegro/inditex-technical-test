4eb4ce3adcb6fff47033a1e27ed390d6c19d1fc2
unknown <axelnperez@hotmail.com>
2024-08-01 23:32:19 +0200

    test: Refactor integration tests and added E2E tests.
    Refactor integration tests and added E2E tests.
    Also, refactor pom.xml to specify versions.

--- technical-test/src/test/java/com/inditex/technicaltest/infrastructure/controller/BrandsControllerIntegrationTest.java
+++ technical-test/src/test/java/com/inditex/technicaltest/infrastructure/controller/BrandsControllerIntegrationTest.java
@@ -1,14 +1,11 @@
 package com.inditex.technicaltest.infrastructure.controller;
 
+import com.inditex.technicaltest.common.BaseTestAnnotation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -19,9 +16,7 @@ import static com.inditex.technicaltest.common.enums.InditexExceptionsEnum.ERROR
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@ExtendWith(SpringExtension.class)
-@ActiveProfiles("test")
+@BaseTestAnnotation
 @WebAppConfiguration
 @Sql("/init.sql")
 class BrandsControllerIntegrationTest {
